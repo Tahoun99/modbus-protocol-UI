eabd459c076bced8085f6dc3335004dae9547fc7
Tahoun99 <103882114+Tahoun99@users.noreply.github.com>
2024-09-05 13:22:31 +0300

    add is working

--- src/app/add-edit-device/add-edit-device.component.ts
+++ src/app/add-edit-device/add-edit-device.component.ts
@@ -10,47 +10,111 @@ export class DeviceFormComponent implements OnInit {
   device = {
     ip: '',
     port: '',
+    type: '',
     name: '',
     model: '',
-    installationDate: '',
-    lifeTime: '',
-    password: '',
-    manufacturCountry: '',
-    branch: ''
+    userName: '',
+    passWord: '',
+    // installation_Date: '2024-05-12T10:45:50.6332225',
+    Life_Time: '',
+    Maunfacturer: '',
+    Made_In_Counry: '',
+    state: 0,
+    branchId: 0
   };
+
+  ngOnInit() {
+  }
 
   constructor(private deviceService: DeviceService) { }
 
   isNewDevice = true;
 
+  // onSubmit() {
+  //   if (this.isNewDevice) {
+  //     if (this.validateDevice(this.device)) {
+  //       console.log(this.device);
+  //       this.deviceService.addDevice(this.device).subscribe(
+  //         (response) => {
+  //           console.log('Device added successfully:', response);
+  //         },
+  //         (error) => {
+  //           console.error('Error adding device:', error);
+  //         }
+  //       );
+  //     } else {
+  //       console.error('Invalid device data');
+  //     }
+  //   } 
+  //   // else {
+  //   //   if (this.validateDevice(this.device) && typeof this.device.id === 'number') {
+  //   //     this.deviceService.updateDevice(this.device.id, this.device).subscribe(
+  //   //       (response) => {
+  //   //         console.log('Device updated successfully:', response);
+  //   //       },
+  //   //       (error) => {
+  //   //         console.error('Error updating device:', error);
+  //   //       }
+  //   //     );
+  //   //   } else {
+  //   //     console.error('Invalid device data or missing device ID');
+  //   //   }
+  //   // }
+  
+  //   console.log('Form submitted:', this.device);
+  // }
+  
   onSubmit() {
     if (this.isNewDevice) {
-      this.deviceService.addDevice(this.device).subscribe(
-        (response) => {
-          console.log('Device added successfully:', response);
-        },
-        (error) => {
-          console.error('Error adding device:', error);
-        }
-      );
+      if (this.validateDevice(this.device)) {
+        console.log(this.device);
+        this.deviceService.addDevice(this.device).subscribe(
+          (response) => {
+            console.log('Device added successfully:', response);
+          },
+          (error) => {
+            console.error('Error adding device:', error);
+          }
+        );
+      } else {
+        console.error('Invalid device data');
+      }
     } 
-    // else {
-    //   this.deviceService.updateDevice(this.device.id, this.device).subscribe(
-    //     (response) => {
-    //       console.log('Device updated successfully:', response);
-    //     },
-    //     (error) => {
-    //       console.error('Error updating device:', error);
-    //     }
-    //   );
-    // }
+  //   else {
+  //     if (this.validateDevice(this.device) && typeof this.device.id === 'number') {
+  //       this.deviceService.updateDevice(this.device.id, this.device).subscribe(
+  //         (response) => {
+  //           console.log('Device updated successfully:', response);
+  //         },
+  //         (error) => {
+  //           console.error('Error updating device:', error);
+  //         }
+  //       );
+  //     } else {
+  //       console.error('Invalid device data or missing device ID');
+  //     }
+  //   }
   
-    console.log('Form submitted:', this.device);
+  //   console.log('Form submitted:', this.device);
   }
-
-
-  ngOnInit() {
+  
+  validateDevice(device: any): boolean {
+    if (
+      typeof device.ip === 'string' &&
+      typeof device.port === 'string' &&
+      typeof device.type === 'string' &&
+      typeof device.name === 'string' &&
+      (device.model === undefined || typeof device.model === 'string') &&
+      (device.userName === undefined || typeof device.userName === 'string') &&
+      (device.passWord === undefined || typeof device.passWord === 'string') &&
+      typeof device.Life_Time === 'string' &&
+      typeof device.Maunfacturer === 'string' &&
+      typeof device.Made_In_Counry === 'string' &&
+      typeof device.state === 'number' &&
+      (device.branchId === undefined || typeof device.branchId === 'number')
+    ) {
+      return true;
+    }
+    return false;
   }
 }
-
-
